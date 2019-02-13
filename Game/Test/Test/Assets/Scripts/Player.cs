4186c47e61483a97603d32b11db26ce29ad8814e
1adrees <achaud207@caledonian.ac.uk>
2019-02-13 19:13:44 +0000

    Test Level 1
    Test Level 1 designed.
    Includes: character movement and walls.
    To do next: enemy ai

---
+++ Game/Test/Test/Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
+        rb2d.AddForce(movement * speed);
+    }
+
+
+}
---
+++ Game/Test/Test/Assets/Scripts/PlayerAnalogue.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnalogue : MonoBehaviour
+{
+
+    public KeyCode moveLeftKey = KeyCode.LeftArrow;
+    public KeyCode moveRightKey = KeyCode.RightArrow;
+    public KeyCode moveUpKey = KeyCode.UpArrow;
+    public KeyCode moveDownKey = KeyCode.DownArrow;
+    bool canMoveLeft = true;
+    bool canMoveRight = true;
+    bool canMoveUp = true;
+    bool canMoveDown = true;
+
+    public float speedx = 0.2f;
+    public float speedy = 0.2f;
+    float directionx = 0.0f;
+    float directiony = 0.0f;
+
+    void FixedUpdate()
+    {
+        Vector3 position = transform.localPosition;
+        position.x += speedx * directionx;
+        position.y += speedy * directiony;
+        transform.localPosition = position;
+    }
+
+    void Update()
+    {
+        bool isLeftPressed = Input.GetKey(moveLeftKey);
+        bool isRightPressed = Input.GetKey(moveRightKey);
+        bool isUpPressed = Input.GetKey(moveUpKey);
+        bool isDownPressed = Input.GetKey(moveDownKey);
+
+        if (isLeftPressed && canMoveLeft)
+        {
+            directionx = -1.0f;
+        }
+        else if (isRightPressed && canMoveRight)
+        {
+            directionx = 1.0f;
+        }
+        else if (isUpPressed && canMoveUp)
+        {
+            directiony = 1.0f;
+        }
+        else if (isDownPressed && canMoveDown)
+        {
+            directiony = -1.0f;
+        }
+        else
+        {
+            directionx = 0.0f;
+            directiony = 0.0f;
+        }
+    }
+}
